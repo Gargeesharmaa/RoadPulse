54282428a64dc161f13d167a62946b1e46df6087
Gargee Sharma <gargeesharma52@gmail.com>
2026-07-18 21:01:11 +0530

    add image router

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from api.upload import router as upload_router
 
 from api.routes import router
 
@@ -20,6 +21,7 @@ app.add_middleware(
 
 # Register all API routes
 app.include_router(router)
+app.include_router(upload_router)
 
 
 @app.get("/")
